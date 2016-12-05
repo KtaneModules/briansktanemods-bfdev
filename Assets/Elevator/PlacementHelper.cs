1fbe4944d223b29221a80d95c829e5d8679538d1
bfdev <brian.fetter.dev@gmail.com>
2016-12-05 10:05:15 -0500

    First checkin for elevator mod

---
+++ Assets/Elevator/PlacementHelper.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlacementHelper : MonoBehaviour
+{
+    public GameObject Prefab;
+    public float PrefabWidth;
+    public float PrefabHeight;
+    public float Width;
+    public float Height;
+
+    [ContextMenu("Place Prefabs")]
+	public void PlacePrefabs()
+    {
+        foreach(Transform t in GetComponentsInChildren<Transform>())
+        {
+            if(t != transform)
+            {
+                Destroy(t.gameObject);
+            }
+        }
+
+        int numX = (int)(Width / PrefabWidth);
+        int numY = (int)(Height / PrefabHeight);
+
+        for(int i = 0; i < numX; i++)
+        {
+            for(int j = 0; j < numY; j++)
+            {
+                GameObject inst = Instantiate(Prefab);
+                inst.transform.parent = transform;
+                inst.transform.localPosition = new Vector3(i * PrefabWidth, 0, -j * PrefabHeight);
+            }
+        }
+    }
+}
