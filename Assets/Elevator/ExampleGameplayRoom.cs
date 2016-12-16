c0a022f2a31eae1b672f79de5bf3b07048da0cc2
Brian Fetter <brian.fetter.dev@gmail.com>
2016-12-16 16:50:05 -0500

    Added lights on event and set up timer to be where floor indicator was.

--- Assets/Elevator/ExampleGameplayRoom.cs
+++ Assets/Elevator/ExampleGameplayRoom.cs
@@ -1,18 +1,24 @@
 ﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class ExampleGameplayRoom : MonoBehaviour
 {
-    public Light roomLight;
+    public List<Light> RoomLights;
 
 	void Awake()
     {
         KMGameplayRoom gameplayRoom = GetComponent<KMGameplayRoom>();
         Debug.Log("Setting on light change");
+        OnLightChange(false);
         gameplayRoom.OnLightChange = OnLightChange;
 	}
 
     public void OnLightChange(bool on)
     {
-        //roomLight.enabled = on;
+        foreach (Light light in RoomLights)
+        {
+            light.enabled = on;
+        }
     }
 }
